90aaf09c165217f9d9629abaab769be951964789
csotot <66373682+csotot@users.noreply.github.com>
2023-06-24 13:27:23 -0500

    final_commit

--- pregunta.py
+++ pregunta.py
@@ -18,43 +18,29 @@ import pandas as pd
 
 
 def ingest_data():
+    cluster = {"text": "cluster", "length": 8}
+    cantidad_de_palabras_clave = {"text": "Cantidad de palabras clave", "length": 15}
+    porcentaje_de_palabras_clave = {"text": "Porcentaje de palabras clave", "length": 16}
+    principales_palabra_clave = {"text": "Principales palabras clave", "length": 79}
+
+
     df = pd.read_fwf(
         "clusters_report.txt",
-        widths=[7, 16, 16, 79],
+        widths=[cluster["length"], cantidad_de_palabras_clave["length"], porcentaje_de_palabras_clave["length"], principales_palabra_clave["length"]],
         header=[0],
         skiprows=[1, 2, 3]
     )
-
-    lastCluster = 1
-    lastPClave = 105
-    lastPorcentaje = "15,9 %"
-    for i, _ in df.iterrows():
-        if df.iloc[i,0] != lastCluster and not pd.isna(df.iloc[i,0]):
-            lastCluster = df.iloc[i,0]
-            lastPClave = df.iloc[i,1]
-            lastPorcentaje = df.iloc[i,2]
-        else:
-            df.iloc[i,0] = lastCluster
-            df.iloc[i,1] = lastPClave
-            df.iloc[i,2] = lastPorcentaje
 
     df = df.groupby(["Cluster", "Cantidad de", "Porcentaje de"])
 
     df = df.agg(lambda x: " ".join(x)).reset_index()
 
-    df["Principales palabras clave"] = df["Principales palabras clave"].str.replace(r"\s{2,}", " ", regex=True)
+    df[principales_palabra_clave["text"]] = df[principales_palabra_clave["text"]].str.replace(r"\s{2,}", " ", regex=True)
+    df[principales_palabra_clave["text"]] = df[principales_palabra_clave["text"]].str.replace(".", "", regex=True)
 
-    df["Principales palabras clave"] = df["Principales palabras clave"].str.replace(".", "", regex=True)
+    df["Porcentaje de"] = df["Porcentaje de"].str.slice(0, -2).str.replace(",", ".").astype(float)
 
-    df["Porcentaje de"] = df["Porcentaje de"].str.slice(0, -2)
-    df["Porcentaje de"] = df["Porcentaje de"].str.replace(",", ".").astype(float)
-
-    df.columns = [
-            "cluster",
-            "cantidad_de_palabras_clave",
-            "porcentaje_de_palabras_clave",
-            "principales_palabras_clave"
-    ]
+    df.columns = ["cluster", "cantidad_de_palabras_clave", "porcentaje_de_palabras_clave", "principales_palabras_clave"]
 
     return df
 
